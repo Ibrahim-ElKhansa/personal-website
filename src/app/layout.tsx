e284d8f00a757a7dd57673e7cbc7b765ce96d51b
Ibrahim El Khansa <itzbahb@gmail.com>
2025-05-29 20:19:55 +0300

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.scss";
 
 const inter = Inter({
@@ -25,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.variable}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
