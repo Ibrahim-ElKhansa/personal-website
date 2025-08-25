f6ff5ab63af4d8a321ff9a2904299ee5971cbc2b
Ibrahim El Khansa <itzbahb@gmail.com>
2025-08-25 23:39:14 +0300

    remoived supression warning (i just disabled grammarly)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning className={inter.variable}>
+      <body className={inter.variable}>
         {children}
         <Analytics />
       </body>
